432de030ecb937eaaed3763a6f432eb12f1cb693
hafidikhsan <harifin315@gmail.com>
2022-03-16 23:15:51 +0700

    Make hero and detail in Detail Page

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -14,6 +14,71 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(restaurant.name),
       ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: <Widget>[
+          ClipRRect(
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(30.0)),
+              child: Image.network(restaurant.pictureId)),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  restaurant.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Text(
+                    restaurant.description,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.star,
+                            size: 26.0,
+                          ),
+                          Text(
+                            restaurant.rating.toString(),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      )),
+                      Expanded(
+                          child: Column(
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_city,
+                            size: 26.0,
+                          ),
+                          Text(
+                            restaurant.city,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      )),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
