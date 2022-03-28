9361c03e792e846a6e1403534d08291fbe68403b
hafidikhsan <harifin315@gmail.com>
2022-03-28 11:54:40 +0700

    Make Share Preference for Schedule Toogle

--- lib/providers/schedule_provider.dart
+++ lib/providers/schedule_provider.dart
@@ -2,14 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:restaurant_app/utils/date_time_helper.dart';
 import 'package:restaurant_app/services/background_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SchedulingProvider extends ChangeNotifier {
+  SchedulingProvider() {
+    _loadNumber();
+  }
   bool _isScheduled = false;
+
+  static const String togglePrefs = 'scheduling';
 
   bool get isScheduled => _isScheduled;
 
   set isScheduled(bool value) {
     _isScheduled = value;
+    notifyListeners();
+    scheduledRestaurants(_isScheduled);
+    _saveToggle();
+  }
+
+  void _saveToggle() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setBool(togglePrefs, _isScheduled);
+  }
+
+  void _loadNumber() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    _isScheduled = prefs.getBool(togglePrefs) ?? false;
     notifyListeners();
     scheduledRestaurants(_isScheduled);
   }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -35,6 +35,7 @@ class _HomePageState extends State<HomePage> {
     _notificationHelper.configureSelectNotificationSubject(
       DetailPage.routeName,
     );
+    SchedulingProvider();
   }
 
   @override
